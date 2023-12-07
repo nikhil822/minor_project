1595e0edaf4af990ca162960aabd2fe53816602b
Nikhil Kumar Sahu <75385653+nikhil822@users.noreply.github.com>
2023-12-07 10:56:52 +0530

    Update APIRoutes.jsx

--- client/src/utils/APIRoutes.jsx
+++ client/src/utils/APIRoutes.jsx
@@ -1,4 +1,4 @@
-export const host = 'http://localhost:5001'
+export const host = 'https://minor-project-wnzz.vercel.app'
 export const registerRoute = `${host}/api/auth/signup`
 export const loginRoute = `${host}/api/auth/login`
 export const nRegisterRoute = `${host}/api/authn/nsignup`
